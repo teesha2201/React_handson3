31a5670a6508bc8ee5ae60c41c0426e089085058
teesha2201 <teesha2201@gmail.com>
2023-09-02 01:37:11 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Formpage from './form/Formpage';
+
+function App() {
+  return (
+    <div className="App">
+        <Formpage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/form/Data.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./Formpage.css"
+class Data extends Component{
+    constructor(props)
+    {
+        super(props)
+    }
+     
+    render(){
+        return(
+            <>
+             <div className="parent">
+                        {this.props.value.map((element,index)=>{
+                            return(
+                            <>
+                                <div key ={index} className="child">  
+                                    <p>Name of Student:{element.name} || Department:{element.department} || Rating:{element.rating}</p>
+                                </div> 
+                            </>   
+                        )
+                        })}
+                    </div>
+                    
+            <button onClick={this.props.value1} className="goback">Go Back</button>
+            </>
+        )
+    }
+}
+export default Data
